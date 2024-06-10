41dffa846e1d6c4ad95d68c4ee26194c821f551e
Boy <52785948+Boy-Stroo@users.noreply.github.com>
2024-06-10 15:43:05 +0200

    Fixed member_id

--- Controller/MemberController.py
+++ Controller/MemberController.py
@@ -5,17 +5,21 @@ import random
 
 
 class MemberController:
-    def generate_member_id(self):
+    def generate_member_id(self) -> str: 
         print("generate_member_id")
         year = datetime.datetime.now().year.__str__()[2:]
-        intyear = int(year[-1])
+        intyear = int(year)
 
         random_numbers = list()
-        random_numbers.append(intyear)
+        # Add first to numbers of the year
+        random_numbers.append(int(year[0]))
+        random_numbers.append(int(year[1]))
 
+        # Add 7 random numbers
         for i in range(0, 7):
             random_numbers.append(random.randint(0, 9))
         
+        # Calculate and add checksum
         checksum = sum(random_numbers) % 10
         random_numbers.append(checksum)
 
@@ -24,3 +28,7 @@ class MemberController:
         print(f'Generated member ID: {member_id}')
         input('Press enter to continue...')
         return member_id
+
+if (__name__ == "__main__"):
+    member_controller = MemberController()
+    member_controller.generate_member_id()
